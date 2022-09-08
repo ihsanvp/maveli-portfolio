6530c0b063621f115b661ec1855e12f896f49db1
ihsanvp <ihsanvp2738@gmail.com>
2022-09-08 17:00:15 +0530

    fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()]
-})
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode == "production" ? "/maveli-portfolio/" : "/",
+    plugins: [svelte()],
+  };
+});
