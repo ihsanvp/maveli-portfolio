6ecf4cb0f6b77f1a59c82574955bede85787f02e
ihsanvp <ihsanvp2738@gmail.com>
2022-09-09 15:01:37 +0530

    make Header2 responsive

--- src/lib/Header2.svelte
+++ src/lib/Header2.svelte
@@ -3,15 +3,21 @@
 </script>
 
 <section>
-  <div class="flex w-full h-screen items-center">
-    <div class="flex-1 flex flex-col items-center justify-center gap-10">
-      <div class="text-8xl font-serif">Lorem ipsum dolor sit amet.</div>
-      <div class="text-xl font-serif pr-56">
+  <div class="flex flex-col lg:flex-row w-full h-screen items-center">
+    <div
+      class="flex-1 flex flex-col items-center lg:justify-center gap-5 lg:gap-10 order-2 lg:order-1 pb-10 lg:pb-0 pt-5 lg:pt-10"
+    >
+      <div class="text-4xl lg:text-8xl font-serif text-center lg:text-left">
+        Lorem ipsum dolor sit amet.
+      </div>
+      <div class="text-base font-serif lg:pr-56 text-center lg:text-left">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates odit
         quos officiis, perferendis eveniet explicabo.
       </div>
     </div>
-    <div class="flex items-center justify-center w-[30vw] max-h-[30vw]">
+    <div
+      class="flex items-center justify-center pt-10 lg:pt-0 lg:w-[30vw] lg:max-h-[30vw] order-1 lg:order-2 pb-5 lg:pb-0"
+    >
       <img class="w-full h-full object-contain" src={maveli} alt="maveli" />
     </div>
   </div>
